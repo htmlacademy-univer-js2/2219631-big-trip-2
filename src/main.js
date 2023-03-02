ce648cd85a54351d47c9627943d3cad684740166
tvsmsa <tmmelekhin@gmail.com>
2023-03-02 23:41:20 +0500

    Время разбивать камни

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import HeaderPresenter from './presenter/header-presenter.js';
+import TripPresenter from './presenter/trip-presenter.js';
+import {render} from './render.js';
+
+
+const tripMainElement = document.querySelector('.trip-main');
+const headerPresenter = new HeaderPresenter(tripMainElement);
+
+const eventElement = document.querySelector('.trip-events');
+const eventPresenter = new TripPresenter(eventElement);
+
+headerPresenter.init();
+eventPresenter.init();
+
---
+++ src/presenter/trip-presenter.js
@@ -0,0 +1,26 @@
+import EventAddView from "../view/event-add-view";
+import EventView from "../view/event-view";
+import EventListView from "../view/event-list-view";
+import EventEditView from "../view/event-edit-view";
+import SortView from "../view/sort-view";
+import EventDestinationView from "../view/event-destination-view";
+import EventOffersView from "../view/event-offers-view";
+import { render, RenderPosition } from "../render";
+
+export default class TripPresenter{
+  constructor(tripContainer){
+    this.tripContainter = tripContainer;
+    this.eventList = new EventListView();
+    this.addEvent = new EventAddView();
+    this.editEvent = new EventEditView();
+  }
+
+  init(){
+    render(new SortView(),this.tripContainter);
+    render(this.editEvent, this.tripContainter);
+    render(this.eventList, this.tripContainter);
+    for (let i=0; i<3; i++){
+      render(new EventView(), this.eventList.getElement());
+    }
+  }
+}
